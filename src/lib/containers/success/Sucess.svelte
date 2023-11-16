43c8143d3b092c7fc0b1d173cd274cafcb04a97f
gbihry <98669194+gbihry@users.noreply.github.com>
2023-11-16 15:18:39 +0100

    ADD : success + home situation page

--- src/lib/containers/success/Sucess.svelte
+++ src/lib/containers/success/Sucess.svelte
@@ -1,7 +1,21 @@
-<script>
+<script lang="ts">
+    import { Button } from "$lib/components/ui/button";
+    import { goto } from '$app/navigation';
 
+    function goHome() {
+        goto('/');
+    }
+
+    function goRetry() {
+        goto('./');
+    }
 </script>
 
-<section>
-    <h1>wesh c'est moi</h1>
+<section class="m-4 mb-0 text-2xl w-full rounded-2xl bg-card p-4 justify-center flex-col gap-10 flex items-center">
+    <h1>Congratulations on passing this module !</h1>
+    <div class="flex flex-row gap-5">
+        <Button on:click={() => goHome()}>Home</Button>
+        <Button variant="destructive" class="text-white" on:click={() => goRetry()}>Retry</Button>
+    </div>
+    
 </section>
---
+++ src/routes/situations/[situationName]/+page.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+    import { Button } from "$lib/components/ui/button";
+    import { goto } from '$app/navigation';
+
+    import { SITUATIONS } from '$lib/DATA';
+    import { page } from '$app/stores';
+    
+
+    let situation = SITUATIONS[$page.params.situationName];
+	$: situation = SITUATIONS[$page.params.situationName];
+
+    let step = situation.steps[$page.params.situationStep];
+	$: step = situation.steps[$page.params.situationStep];
+
+    $: actualSituation = $page.params.situationName;
+
+    function start() {
+        let etapes = Object.keys(situation.steps);
+        console.log(etapes[0]);
+        
+        goto('./' + actualSituation + "/" + etapes[0]); 
+    }
+
+</script>
+
+<section class="m-4 mb-0 text-2xl w-full rounded-2xl bg-card p-4 justify-center flex-col gap-10 flex items-center">
+    <h1>You chose the next situation : {situation.nom}</h1>
+    <Button on:click={() => start()}>Start game</Button>
+    
+</section>
